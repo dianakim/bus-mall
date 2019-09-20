0e2304c12017b56fb79b831c02e7be0d3f8e23a0
Diana Kim <dianajkim@gmail.com>
2019-09-19 18:37:19 -0700

    Picture constructor function

--- js/app.js
+++ js/app.js
@@ -2,38 +2,39 @@
 
 var canvasEl = document.getElementById('my-canvas');
 
-var picContainerEl = document.getElementById('pic-container');
-
 var firstImgEl = document.getElementById('firstImg');
-
 var secondImgEl = document.getElementById('secondImg');
-
 var thirdImgEl = document.getElementById('thirdImg');
 
 var allPics = [];
 
 function Picture(name) {
-  this.url = '../img/${name}.';
-  this.src = name;
+  this.src = '../img/${name}.jpg';
   this.alt = name;
+  this.title = name;
+  this.shown = '0';
+  this.clicked = '0';
 
   allPics.push(this);
 }
 
-// var newGraph = document.getElementById('bar-chart');
-
-// new Chart(newGraph).baseURI(barData);
-
-// var barData = {
-//   labels: ['one', 'two', 'three', 'four'],
-//   datasets: [
-//     {
-//       fillColor: 'rgba(172, 194, 132, 0.5)',
-//       strokeColor: '#ACC260',
-//       pointColor: '#fff',
-//       pointStrokeColor: '#9DB86D',
-//       data: [203, 156, 99, 251, 305, 247]
-//     }
-//   ]
-// };
-
+new Picture('baby-sweep-onesie');
+new Picture('banana-cutter');
+new Picture('bathroom-stand');
+new Picture('canned-unicorn-meat');
+new Picture('canned-dragon-meat');
+new Picture('cthulhu-toy');
+new Picture('duck-muzzle');
+new Picture('lump-chair');
+new Picture('meatball-bubblegum');
+new Picture('modern-wine-goblet');
+new Picture('pen-cutlery');
+new Picture('pet-sweep');
+new Picture('pizza-scissor-spatula');
+new Picture('R2D2-rolling-bag');
+new Picture('shark-sleeping-bag');
+new Picture('tauntaun-sleeping-bag');
+new Picture('toaster-coffee-maker');
+new Picture('toeless-galoshes');
+new Picture('usb-tentacle');
+new Picture('warped-watering-can');
